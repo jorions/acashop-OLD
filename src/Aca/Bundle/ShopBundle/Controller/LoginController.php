9e6db814aee4bd6a20affefe5885387fe88b9ccd
vagrant <vagrant@aca.vm>
2015-10-28 02:05:05 +0000

    Added more to login form, added base templating, added bootstrap

--- src/Aca/Bundle/ShopBundle/Controller/LoginController.php
+++ src/Aca/Bundle/ShopBundle/Controller/LoginController.php
@@ -12,55 +12,69 @@ use Aca\Bundle\ShopBundle\Db\Database;
 
 class LoginController extends Controller {
 
+    /**
+     * Login form action
+     * @param Request $req
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function loginFormAction(Request $req)
     {
-
-        $session = $this->get('session');
-
         $msg = null;
 
+
+        // Sets the $session variable by getting the session
+        $session = $this->getSession();
         $username = $req->get('username');
         $password = $req->get('password');
 
+        if (!empty($username) && !empty($password)) {
 
+            $query = "
+            SELECT
+                user_id
+            FROM
+                aca_user
+            WHERE
+                username='$username'
+                and password='$password'";
 
-        $query = "
-        SELECT
-            user_id
-        FROM
-            aca_user
-        WHERE
-            username='$username'
-            and password='$password'";
+            $db = new Database();
 
-        $db = new Database();
+            $data = $db->fetchRowMany($query);
 
-        $data = $db->fetchRowMany($query);
+            // Invalid login (check for empty username and password)
+            if (empty($data) && $req->getMethod() == 'POST') {
 
-        // Invalid login (check for empty username and password because
-        if(empty($data) && !empty($username) && !empty($password)) {
+                $msg = 'Please check your credentials';
+                $session->set('loggedIn', false);
 
-            $msg = 'Please check your credentials';
-            $session->set('isLoggedIn', false);
-            $session->save();
-        } else {
+            // Valid login
+            } else {
 
-            // Take the last user of the
-            $row = array_pop($data);
+                // Take the last user of the returned array
+                $row = array_pop($data);
 
-            // User's name
-            $name = $row['name'];
+                // Get user's name
+                $name = $row['name'];
 
-            $session->set('isLoggedIn', true);
-            $session->set('name', $name);
-            $session->save();
+                $session->set('loggedIn', true);
+                $session->set('name', $name);
+            }
         }
 
-        $loggedIn = $session->get('isLoggedIn');
+        // Before you can run any operations on $session you have to save it
+        $session->save();
+
+        $loggedIn = $session->get('loggedIn');
+
+        // If you "get" something that doesn't exist then it will be created with a null value
         $name = $session->get('name');
 
+        // "AcaShopBundle" is the shorthand for the namespace "ACA\Bundle\ShopBundle\Resources\views"
+        // This returns a "response" object, which is the only thing that Symfony can display in the browser
+        // There are 3 types of response objects: Redirect, JSON, and Response
         return $this->render(
-            'AcaShopBundle:LoginForm2:login.html.twig',
+            'AcaShopBundle:LoginForm:login.html.twig',
             array(
                 'loggedIn' => $loggedIn,
                 'name' => $name,
@@ -71,13 +85,35 @@ class LoginController extends Controller {
         );
     }
 
-    public function logout()
+    /**
+     * Get a started session
+     * @return \Symfony\Component\HttpFoundation\Session\Session
+     */
+    public function getSession()
     {
         $session = $this->get('session');
 
-        $session->set('isLoggedIn', false);
+        if(!$session->isStarted()) {
+            $session->start();
+        }
+
+        return $session;
+    }
+
+    /**
+     * Logout logic
+     * @return RedirectResponse
+     */
+    public function logoutAction()
+    {
+        $session = $this->getSession();
+
+        $session->remove('loggedIn');
         $session->remove('name');
 
-        return $this->render('AcaShopBundle:LoginForm2:login.html.twig');
+        $session->save();
+
+        // This is another type of response object
+        return new RedirectResponse('/login_form');
     }
 }
