f1fb50e9c8ae39b1b4b32f2577a82f211b771b7a
jorions <jared.orion.selcoe@gmail.com>
2015-11-16 23:28:52 -0600

    Finished profile controller
    Finished profile twig

--- src/Aca/Bundle/ShopBundle/Controller/ProfileController.php
+++ src/Aca/Bundle/ShopBundle/Controller/ProfileController.php
@@ -4,11 +4,13 @@ namespace Aca\Bundle\ShopBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-// Use this for logoutAction
-use Symfony\Component\HttpFoundation\RedirectResponse;
-
 // Use this for loginFormAction
 use Symfony\Component\HttpFoundation\Request;
+
+// WHY DO THESE SHOW AS UNUSED
+use Aca\Bundle\ShopBundle\Service\ProfileService;
+
+use Aca\Bundle\ShopBundle\Service\LoginService;
 
 class ProfileController extends Controller {
 
@@ -20,27 +22,237 @@ class ProfileController extends Controller {
         // If they are logged in render page
         if($loggedIn) {
 
+            // Set initial error messages to null
+            $shippingMsg = null;
+            $billingMsg = null;
+            $nameMsg = null;
+            $usernameMsg = null;
+            $passwordMsg = null;
+
             $profile = $this->get('profile');
 
-            // If profile page is visited via GET it is a link click, not a form submission
-            if($req->getMethod() == 'GET') {
+            // If profile page is visited via POST it is a form submission
+            // Check for if updateShipping button was pressed
+            if($req->getMethod() == 'POST' && !empty($req->get('updateShipping'))) {
 
-            // If profile page is visited via not GET (POST) it is a form submission
+                // Set render variables
+                $shippingStreet = $req->get('shippingStreet');
+                $shippingCity = $req->get('shippingCity');
+                $shippingState = $req->get('shippingState');
+                $shippingZip = $req->get('shippingZip');
+
+                // Make sure all fields have content
+                if(!empty($shippingStreet) && !empty($shippingCity) && !empty($shippingState) && !empty($shippingZip)) {
+
+                    // Make sure zip is valid
+                    if(preg_match("#^[0-9]+$#", $req->get('shippingZip'))) {
+
+                        // With all checks set, update shipping
+                        $profile->setShippingAddress($shippingStreet, $shippingCity, $shippingState, $shippingZip);
+                        $shippingMsg = 'Shipping address successfully updated!';
+
+                    // If zip is invalid set $msg to error
+                    } else {
+                        $shippingMsg = 'Please enter only numbers for zip';
+                    }
+
+                // If all fields don't have content set $msg to error
+                } else {
+                    $shippingMsg = 'Please enter a street, city, state, and zip';
+                }
+
+            // If profile page is visited not via POST (GET) it is a normal page visit, so set render variables accordingly
             } else {
+                $data = $profile->getShippingAddress();
 
+                $shippingStreet = $data['street'];
+                $shippingCity = $data['city'];
+                $shippingState = $data['state'];
+                $shippingZip = $data['zip'];
             }
 
 
-            $id = $profile->setBillingAddress("456 Kane Street", "Austin", "TX", "73442");
 
-            $address = $profile->getBillingAddress();
+            // Now check for if updateBilling button was pressed
+            if($req->getMethod() == 'POST' && !empty($req->get('updateBilling'))) {
 
+                // Set render variables
+                $billingStreet = $req->get('billingStreet');
+                $billingCity = $req->get('billingCity');
+                $billingState = $req->get('billingState');
+                $billingZip = $req->get('billingZip');
+
+                // Make sure all fields have content
+                if(!empty($billingStreet) && !empty($billingCity) && !empty($billingState) && !empty($billingZip)) {
+
+                    // Make sure zip is valid
+                    if(preg_match("#^[0-9]+$#", $req->get('billingZip'))) {
+
+                        // With all checks set, update billing
+                        $profile->setBillingAddress($billingStreet, $billingCity, $billingState, $billingZip);
+                        $billingMsg = 'Billing address successfully updated!';
+
+                        // If zip is invalid set $msg to error
+                    } else {
+                        $billingMsg = 'Please enter only numbers for zip';
+                    }
+
+                    // If all fields don't have content set $msg to error
+                } else {
+                    $billingMsg = 'Please enter a street, city, state, and zip';
+                }
+
+            // If profile page is visited not via POST (GET) it is a normal page visit, so set render variables accordingly
+            } else {
+                $data = $profile->getBillingAddress();
+
+                $billingStreet = $data['street'];
+                $billingCity = $data['city'];
+                $billingState = $data['state'];
+                $billingZip = $data['zip'];
+            }
+
+
+
+            // Get all username info
+            $userInfo = $profile->getUserInfo();
+            $name = $userInfo['name'];
+            $username = $userInfo['username'];
+            $password = $userInfo['password'];
+
+
+
+            // Now check for if updateName button was pressed
+            if($req->getMethod() == 'POST' && !empty($req->get('updateName'))) {
+
+                $name = $req->get('name');
+
+                // Make sure info was entered
+                if(!empty($name)) {
+
+                    // Prevent MySQL injection - make sure all characters are legal
+                    if(preg_match("#^[a-zA-Z0-9]+$#", $name)) {
+
+                        // Set new name
+                        $profile->updateName($name);
+                        $nameMsg = 'Name updated!';
+
+                    // If any illegal characters tell user
+                    } else {
+                        $nameMsg = 'Make sure your name contains only letters and numbers';
+                    }
+
+                // If name empty set error message
+                } else {
+
+                    $nameMsg = 'No new name entered';
+                }
+            }
+
+
+
+            // Now check for if updateUsername button was pressed
+            if($req->getMethod() == 'POST' && !empty($req->get('updateUsername'))) {
+
+                $username = $req->get('username');
+
+                // Make sure info was entered
+                if(!empty($username)) {
+
+                    // Prevent MySQL injection - make sure all characters are legal
+                    if (preg_match("#^[a-zA-Z0-9]+$#", $username)) {
+
+                        // Make sure username isn't already used
+                        $login = $this->get('login');
+                        if($login->checkRegistration($username)) {
+
+                            // Set new username
+                            $profile->updateUsername($username);
+                            $usernameMsg = 'Username updated!';
+
+                        // If username is same as original tell user (instead of giving error message)
+                        } else if($username == $userInfo['username']) {
+
+                            $usernameMsg = 'That\'s your current username!';
+
+                        // If username already exists tell user
+                        } else {
+
+                            $usernameMsg = 'That username is already taken - sorry!';
+                        }
+
+                    // If any illegal characters tell user
+                    } else {
+                        $usernameMsg = 'Make sure your username contains only letters and numbers';
+                    }
+
+                // If username empty set error message
+                } else {
+
+                    $usernameMsg = 'No new username entered';
+                }
+            }
+
+
+
+            // Now check for if updatePassword button was pressed
+            if($req->getMethod() == 'POST' && !empty($req->get('updatePassword'))) {
+
+                $password = $req->get('password');
+                $passwordCheck = $req->get('passwordCheck');
+
+                // Make sure info was entered
+                if(!empty($password) && !empty($passwordCheck)) {
+
+                    // Prevent MySQL injection - make sure all characters are legal
+                    if(preg_match("#^[a-zA-Z0-9]+$#", $password) && preg_match("#^[a-zA-Z0-9]+$#", $req->get('updatePassword'))) {
+
+                        // Make sure passwords match
+                        if($password == $passwordCheck) {
+
+                            // Set new password
+                            $profile->updatePassword($password);
+                            $passwordMsg = 'Password updated!';
+
+                        // If passwords don't match tell user
+                        } else {
+                            $passwordMsg = 'Make sure your new password matches in both boxes';
+                        }
+
+                    // If any illegal characters tell user
+                    } else {
+                        $passwordMsg = 'Make sure your password contains only letters and numbers';
+                    }
+
+                // If one of the password fields was empty set error message
+                } else {
+
+                    $passwordMsg = 'Enter your new password in both boxes';
+                }
+            }
+
+
+
+            // Render the final page with all variables
             return $this->render(
                 'AcaShopBundle:Profile:profile.html.twig',
                 array(
-                    'id' => $id,
                     'loggedIn' => $loggedIn,
-                    'address' => $address
+                    'shippingMsg' => $shippingMsg,
+                    'shippingStreet' => $shippingStreet,
+                    'shippingCity' => $shippingCity,
+                    'shippingState' => $shippingState,
+                    'shippingZip' => $shippingZip,
+                    'billingMsg' => $billingMsg,
+                    'billingStreet' => $billingStreet,
+                    'billingCity' => $billingCity,
+                    'billingState' => $billingState,
+                    'billingZip' => $billingZip,
+                    'nameMsg' => $nameMsg,
+                    'name' => $name,
+                    'usernameMsg' => $usernameMsg,
+                    'username' => $username,
+                    'passwordMsg' => $passwordMsg,
                 )
             );
 
@@ -54,7 +266,5 @@ class ProfileController extends Controller {
                 )
             );
         }
-
     }
-
 }
